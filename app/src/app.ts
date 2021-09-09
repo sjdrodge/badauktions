26715d493df34b90f428f8bba8e065546fc068bb
Stephen Drodge <stephen.drodge@gmail.com>
2021-09-08 23:35:04 -0400

    Move to a more default prettier configuration.

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,18 +1,18 @@
-import express from 'express';
-import redis from 'redis';
+import express from "express";
+import redis from "redis";
 
 const client = redis.createClient({
-  host: 'redis',
+  host: "redis",
 });
 const app = express();
 const port = process.env.PORT || 3000;
 
-client.on('error', (error) => {
+client.on("error", (error) => {
   console.error(error);
 });
 
-app.get('/', (req, res) => {
-  client.incr('visits', (err, visits) => {
+app.get("/", (req, res) => {
+  client.incr("visits", (err, visits) => {
     res.send(`<h1>Hello, World!</h1> ${visits} visits to this page so far!`);
   });
 });
@@ -31,12 +31,12 @@ function shutdown() {
   });
 }
 
-const signals = ['SIGINT', 'SIGTERM'];
+const signals = ["SIGINT", "SIGTERM"];
 for (const signal of signals) {
   process.on(signal, () => {
     console.info(
       `Received ${signal}. Shutting down gracefully...`,
-      new Date().toISOString(),
+      new Date().toISOString()
     );
     shutdown();
   });
