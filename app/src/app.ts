ee20ed0bedc45495742953c0586651fd694c2d17
Stephen Drodge <stephen.drodge@gmail.com>
2021-09-11 04:03:15 -0400

    Enabled websockets via socket.io.

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,23 +1,49 @@
 import express from "express";
+import http from "http";
 import redis from "redis";
+import socketIO from "socket.io";
 
-const client = redis.createClient({
+const redisClient = redis.createClient({
   host: "redis",
 });
 const app = express();
 const port = process.env.PORT || 3000;
 
-client.on("error", (error) => {
+redisClient.on("error", (error) => {
   console.error(error);
 });
 
 app.get("/", (req, res) => {
-  client.incr("visits", (err, visits) => {
-    res.send(`<h1>Hello, World!</h1> ${visits} visits to this page so far!`);
+  redisClient.incr("visits", (err, visits) => {
+    res.send(`<!DOCTYPE html>
+<html>
+<head>
+<script src="/socket.io/socket.io.js"></script>
+<script>
+const socket = io();
+</script>
+</head>
+<body>
+<h1>Hello, World!</h1>
+${visits} visits to this page so far!
+</body>
+</html>`);
   });
 });
 
-const server = app.listen(port, () => {
+const server = http.createServer(app);
+const io = new socketIO.Server(server, {});
+
+io.on("connection", (socket) => {
+  console.log(
+    `New socket.io connection: ${socket.id} from ${socket.handshake.address}`,
+  );
+  socket.on("disconnect", (reason) => {
+    console.log(`Closed socket.io connection: ${socket.id} due to "${reason}"`);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Badauktions listening at http://localhost:${port}`);
 });
 
